2c1ab374b6c3bfb570a81249e93e9a3a99ce04b5
murilopacos <murilopacos@gmail.com>
2022-04-24 14:09:36 -0300

    adicionando print mensagem qualquer

--- FirstProgram/src/Program.java
+++ FirstProgram/src/Program.java
@@ -2,5 +2,6 @@ public class Program {
     public static void main(String[] args) {
         System.out.println("Olá Mundo!!!");
         System.out.println("Murilo dos Paços");
+        System.out.println("Vamos com tudo!!!");
     }
 }
