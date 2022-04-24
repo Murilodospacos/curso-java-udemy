7838f7d9ddf6f56eb72da91ac4f8766fb32eb8d1
murilopacos <murilopacos@gmail.com>
2022-04-24 15:44:37 -0300

    exemplo1 garbage collector

---
+++ CodigosAulaSecao10/src/Program.java
@@ -0,0 +1,9 @@
+public class Program {
+    public static void main(String[] args) {
+        int x = 10;
+        if (x > 0) {
+            int y = 20;
+        }
+        System.out.println(x);
+    }
+}
